5ece686c115cfd983b76b83a15b793340c1f7644
junghee.kim <wjdgml3834@naver.com>
2022-02-16 11:29:50 +0900

    ✨  18.2.1 미들웨어 만들기

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 import rootReducer from "./modules";
+import loggerMiddleware from "./lib/loggerMiddleware";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
